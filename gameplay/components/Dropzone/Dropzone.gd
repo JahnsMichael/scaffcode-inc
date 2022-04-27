0d9dad7f4d8bddbceffc3b93d432146e717b5810
Jahns Michael <michael.jf.jm@gmail.com>
2022-04-27 18:17:23 +0700

    add validation button func

--- gameplay/components/Dropzone/Dropzone.gd
+++ gameplay/components/Dropzone/Dropzone.gd
@@ -1,14 +1,18 @@
 extends MarginContainer
 
 export(int) var tile_size = 100
+export(String, FILE, "*.png") var answer_texture_path
 
 onready var _icon = $VBoxContainer/Icon
 var row_index
 var column_index
+var answer_texture
 
 
 func _ready() -> void:
 	rect_min_size = Vector2(tile_size, tile_size)
+	answer_texture = load(answer_texture_path)
+	$Background.color = Color.gray
 
 func can_drop_data(_position: Vector2, draggable) -> bool:
 	var can_drop: bool = draggable is Node and draggable.is_in_group("DRAGGABLE")
@@ -23,3 +27,10 @@ func _on_ClearButton_pressed():
 	
 func clear():
 	_icon.texture = null
+	$Background.color = Color.gray
+
+func validate():
+	if _icon.texture != answer_texture :
+		$Background.color = Color.firebrick
+	else:
+		$Background.color = Color.aquamarine
