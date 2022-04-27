5b212b06368249832473edca665c0895cea01110
Jahns Michael <michael.jf.jm@gmail.com>
2022-04-27 11:31:01 +0000

    Add validation button mechanics

--- gameplay/components/AutomationPhase/AutomationPhase.gd
+++ gameplay/components/AutomationPhase/AutomationPhase.gd
@@ -4,7 +4,11 @@ var dropzones
 
 func _ready():
 	dropzones = get_tree().get_nodes_in_group("dropzone")
-
+		
 func _on_Reset_pressed():
 	for dropzone in dropzones:
 		dropzone.clear()
+
+func _on_Run_pressed():
+	for dropzone in dropzones:
+		dropzone.validate()
