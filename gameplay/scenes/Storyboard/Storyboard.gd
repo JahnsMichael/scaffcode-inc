0f23c050af979d363567291dcf1952a88011a5a6
Jahns Michael <michael.jf.jm@gmail.com>
2022-05-17 00:54:26 +0700

    add Storyboard scene

---
+++ gameplay/scenes/Storyboard/Storyboard.gd
@@ -0,0 +1,20 @@
+extends Control
+
+func _ready():
+	start_dialog()
+
+func start_dialog():
+	var dialog = Dialogic.start("Storyboard")
+	dialog.pause_mode = PAUSE_MODE_PROCESS
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	self.add_child(dialog)
+	dialog.connect("timeline_end", self, "end_dialog")
+	dialog.connect("dialogic_signal", self, "dialog_listener")
+	get_tree().paused = true
+
+func dialog_listener(_string):
+	SceneChanger.change_scene("res://gameplay/scenes/Maps/JuiceBar.tscn")
+
+func end_dialog(data):
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
