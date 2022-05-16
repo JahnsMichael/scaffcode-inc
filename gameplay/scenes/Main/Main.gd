ae60b8fb21e33606661668f0f8098baf22fd1727
Jahns Michael <michael.jf.jm@gmail.com>
2022-05-17 00:53:46 +0700

    change play scene to storyboard

--- gameplay/scenes/Main/Main.gd
+++ gameplay/scenes/Main/Main.gd
@@ -3,5 +3,9 @@ extends Control
 export(PackedScene) var play_scene
 export(PackedScene) var credit_scene
 
+func _ready():
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _on_PlayButton_pressed():
 	SceneChanger.change_scene_to(play_scene)
